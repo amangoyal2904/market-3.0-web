4dead39514dbda1dddb880a94b09a16a36c58920
Ujjwal Paul <ujjwal.paul@timesinternet.in>
2024-01-30 15:57:53 +0530

    header changes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,30 @@
+import Image from 'next/image';
 import React from 'react'
+import styles from './Header.module.scss'
+import Link from 'next/link';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header id={styles.pageTopbar}>
+      <div className={styles.navbarHeader}>
+        <div className="dflex align-item-center">
+          <Image 
+            src="/logo.png"
+            width={138}
+            height={24}
+            alt="ET Markets"
+            />
+            <div className="dflex" id={styles.searchBar}>
+              <input autoComplete="off" name="ticker_newsearch" className={styles.inputBox} placeholder="Search Stocks, News, Mutual Funds, Crypto etc..." type="text" />
+            </div>
+        </div>        
+        <div className="dflex align-item-center">
+          <Link className="default-btn" href="/watchlist">My Watchlist</Link>
+          <Link className="default-btn" href="/plans.cms" data-ga-onclick="Subscription Flow#SYFT#ATF - url">Subscribe</Link>
+          <div className={styles.defaultLink} data-ga-onclick="ET Login#Signin - Sign In - Click#ATF - url">Sign In</div>
+        </div>
+      </div>
+    </header>
   )
 }
 
