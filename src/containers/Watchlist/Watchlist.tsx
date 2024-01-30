1471e82a513c6e99bae0d29eedc23e40323c0ecf
rohitazad <ram9953878727@gmail.com>
2024-01-30 18:24:56 +0530

    Watch list tab and table html structure

--- src/containers/Watchlist/Watchlist.tsx
+++ src/containers/Watchlist/Watchlist.tsx
@@ -1,7 +1,39 @@
+import styles from './Watchlist.module.scss';
+import WatchListTabs from "../../components/WatchListTabs";
+import WatchListTable from "../../components/WatchListTable";
+
+const TabData = [
+  {
+    name:"Overview",
+    active:"active"
+  },
+  {
+    name:"Key Metrics",
+  },
+  {
+    name:"Returns",
+  },
+  {
+    name:"Volatility",
+  },
+  {
+    name:"Technicals",
+  },
+  {
+    name:"Margins More",
+  },
+  {
+    name:"More",
+  }
+]
 
 const Watchlist = () => {
   return (
-    <div>Watchlist</div>
+    <div className={styles.wraper}>
+      <h1 className={styles.heading1}>Watchlist</h1>
+      <WatchListTabs data={TabData}/>
+      <WatchListTable />
+    </div>
   )
 }
 
