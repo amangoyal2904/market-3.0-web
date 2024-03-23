168383e85b53fc9e36658c7b6c775fe8f0ee17b0
Ujjwal Paul <ujjwal.paul@timesinternet.in>
2024-03-23 12:16:58 +0530

    interrim commit

--- src/app/markets/market-moods/[index]/page.tsx
+++ src/app/markets/market-moods/[index]/page.tsx
@@ -52,7 +52,7 @@ const MarketMoods = async ({ params }: any) => {
       advanceDecline={advanceDeclineData}
       periodic={periodicData}
       allFilters={allFilters}
-      isprimeuser={true}
+      isprimeuser={isprimeuser}
     />
   );
 };
