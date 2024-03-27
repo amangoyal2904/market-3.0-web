15050c6499c0201ee7973a6d34d763a86a35971e
Maneet Rana <maneet.rana@timesinternet.in>
2024-03-27 10:07:30 +0530

    working on stock recos

--- src/app/stocksrecos/[[...slug]]/page.tsx
+++ src/app/stocksrecos/[[...slug]]/page.tsx
@@ -3,10 +3,165 @@ import styles from "./styles.module.scss";
 import APIS_CONFIG from "../../../network/api_config.json";
 import { APP_ENV, getFundHouseInfo, getStockRecosDetail } from "@/utils";
 import service from "@/network/service";
-import { fetchSelectedFilter, getSearchParams } from "@/utils/utility";
+import {
+  fetchSelectedFilter,
+  fnGenerateMetaData,
+  getSearchParams,
+} from "@/utils/utility";
 import Disclaimer from "@/components/StockRecosListing/Disclaimer";
 import { redirect } from "next/navigation";
 import { headers } from "next/headers";
+import { Metadata, ResolvingMetadata } from "next";
+
+const StockRecosMeta = (activeApi: any, niftyFilterData: any, slug: any) => {
+  // console.log("StockRecosHeadTitle----", activeApi, niftyFilterData, slug);
+  const fundHouseInfo = getFundHouseInfo("", slug);
+  switch (activeApi) {
+    case "overview":
+      return {
+        title: `Stock Recommendations ${
+          niftyFilterData.name != "All Stocks"
+            ? " in " + niftyFilterData.name
+            : ""
+        } `,
+        desc: `Stock Recommendations ${
+          niftyFilterData.name != "All Stocks"
+            ? " in " + niftyFilterData.name
+            : ""
+        }: Checkout stock recommendations and advices to find best Stock Recommendations stocks on The Economic Times`,
+        keywords: `Stock Recommendations ${
+          niftyFilterData.name != "All Stocks"
+            ? " in " + niftyFilterData.name
+            : ""
+        }, Stock Recommendations, Stock Analysis, Stock Recommendations ${
+          niftyFilterData.name != "All Stocks"
+            ? " in " + niftyFilterData.name
+            : ""
+        } stocks`,
+      };
+    case "newRecos":
+      return {
+        title: `New ${slug?.[1] != "all" ? slug?.[1] : ""} Recommendations ${
+          niftyFilterData.name != "All Stocks"
+            ? " in " + niftyFilterData.name
+            : ""
+        } `,
+        desc: `New ${slug?.[1] != "all" && slug?.[1]} Recommendations ${
+          niftyFilterData.name != "All Stocks"
+            ? " in " + niftyFilterData.name
+            : ""
+        }: Checkout stock recommendations and advices to find best New ${slug?.[1] != "all" && slug?.[1]} Recommendations stocks on The Economic Times `,
+        keywords: `New ${slug?.[1] != "all" && slug?.[1]} Recommendations, Stock Recommendations, Stock Analysis, New ${slug?.[1] != "all" && slug?.[1]} Recommendations Stocks`,
+      };
+    case "mostBuy":
+      return {
+        title: `Most Buys Stock Recommendations ${
+          niftyFilterData.name != "All Stocks"
+            ? " in " + niftyFilterData.name
+            : ""
+        } `,
+        desc: `Most Buys Stock Recommendations ${
+          niftyFilterData.name != "All Stocks"
+            ? " in " + niftyFilterData.name
+            : ""
+        }: Checkout stock recommendations and advices to find best Most Buys stocks on The Economic Times `,
+        keywords: `Most Buys, Stock Recommendations, Stock Analysis, Most Buys Stocks`,
+      };
+    case "mostSell":
+      return {
+        title: `Most Sells Stock Recommendations ${
+          niftyFilterData.name != "All Stocks"
+            ? " in " + niftyFilterData.name
+            : ""
+        } `,
+        desc: `Most Sells Stock Recommendations ${
+          niftyFilterData.name != "All Stocks"
+            ? " in " + niftyFilterData.name
+            : ""
+        }: Checkout stock recommendations and advices to find best Most Sells stocks on The Economic Times `,
+        keywords: `Most Sells, Stock Recommendations, Stock Analysis, Most Sells Stocks`,
+      };
+    case "recoOnWatchlist":
+      return {
+        title: `Stock Recommendations on Your Watchlist `,
+        desc: `Stock Recommendations on Your Watchlist:  Checkout stock recommendations and advices to find best stocks on The Economic Times`,
+        keywords: `Stock Recommendations on Your Watchlist, Stock Recommendations, Stock Analysis`,
+      };
+    case "recoByFH":
+      return {
+        title: `Stock Recommendations by Brokerages `,
+        desc: `Stock recommendations by fund house: Checkout stock recommendations and advices to find best stocks on The Economic Times`,
+        keywords: `Stock Recommendations, Stock Analysis`,
+      };
+    case "FHDetail":
+      return {
+        title: `Brokerages | ${fundHouseInfo.fundHounseName} ${slug?.[2]} ${
+          niftyFilterData.name != "All Stocks"
+            ? " in " + niftyFilterData.name
+            : ""
+        } `,
+        desc: `${fundHouseInfo.fundHounseName} ${slug?.[2]} Stock Recommendations: Checkout stock recommendations and advices to find best ${slug?.[2]} stocks from ${fundHouseInfo.fundHounseName} on The Economic Times`,
+        keywords: `${fundHouseInfo.fundHounseName}, ${fundHouseInfo.fundHounseName} ${slug?.[2]}, Stock Recommendations, Stock Analysis
+        ${slug?.[2]} Stocks,  ${fundHouseInfo.fundHounseName} Stocks, ${fundHouseInfo.fundHounseName} ${slug?.[2]} Stocks`,
+      };
+    default:
+      return {
+        title: `Stock Recommendations `,
+        desc: `Checkout stock recommendations and advices to find best Stock Recommendations stocks on The Economic Times`,
+        keywords: `Stock Recommendations, Stock Analysis`,
+      };
+  }
+};
+
+export async function generateMetadata(
+  {
+    params,
+    searchParams,
+  }: {
+    params: {
+      slug: string[];
+    };
+    searchParams: any;
+  },
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const headersList = headers();
+  const intFilter = searchParams?.filter ? parseInt(searchParams.filter) : 0;
+  const niftyFilterData = await fetchSelectedFilter(intFilter);
+  const pageUrl = headersList.get("x-url") || "";
+
+  const { slug } = params || [];
+
+  // =====  Get Left Nav Data =======
+  const RECOS_NAV_Link = (APIS_CONFIG as any)["STOCK_RECOS_NAV"][APP_ENV];
+  const recosNavPromise = await service.get({
+    url: RECOS_NAV_Link,
+    params: {},
+  });
+
+  const recosNavResult = await recosNavPromise?.json();
+
+  const getApiType = () => {
+    const activeObj = recosNavResult?.tabs.filter(
+      (item: any) => item.seoPath == slug?.[0],
+    );
+    return slug.includes("fundhousedetails") && slug.length > 1
+      ? "FHDetail"
+      : slug.includes("fundhousedetails")
+        ? "recoByFH"
+        : activeObj[0]?.apiType;
+  };
+
+  const metaDetail = StockRecosMeta(getApiType(), niftyFilterData, slug);
+  const meta = {
+    title: metaDetail.title,
+    desc: metaDetail.desc,
+    keywords: metaDetail.keywords,
+    pathname: pageUrl,
+    index: niftyFilterData.indexId == 0 ? false : true,
+  };
+  return fnGenerateMetaData(meta);
+}
 
 export default async function stocksrecos({
   params,
@@ -17,13 +172,8 @@ export default async function stocksrecos({
   };
   searchParams: any;
 }) {
-  console.log("params.slug ----> ", searchParams);
   const headersList = headers();
-  // read the custom x-url header
   const header_url = headersList.get("x-url") || "";
-
-  console.log("headersList---", headersList);
-  console.log("header_url---", header_url);
 
   const intFilter = searchParams?.filter ? parseInt(searchParams.filter) : 0;
   const selectedFilter = await fetchSelectedFilter(intFilter);
