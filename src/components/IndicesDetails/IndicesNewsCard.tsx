51acefa5fe8e867a8a14eaaedf7cbf580cb0c93e
Ujjwal Paul <ujjwal.paul@timesinternet.in>
2024-04-18 11:53:16 +0530

    indices interrim commit

--- src/components/IndicesDetails/IndicesNewsCard.tsx
+++ src/components/IndicesDetails/IndicesNewsCard.tsx
@@ -1,19 +1,25 @@
-import Image from "next/image";
 import styles from "./IndicesDetails.module.scss";
+import Link from "next/link";
 export const IndicesNewsCard = ({ data }: any) => {
+  const updateURLDimensions = (url: string) => {
+    const regex = /width=\d+&height=\d+/;
+    const replacement = `width=66&height=55`;
+    const newUrl = url.replace(regex, replacement);
+    return "https://img.etimg.com" + newUrl;
+  };
   return (
-    <div className={styles.newsCard}>
+    <Link className={styles.newsCard} href={data.wu} target="_blank">
       <div className={styles.content}>
         <p className={styles.headline}>{data.hl}</p>
         <span>03 Oct, 2023</span>
       </div>
-      <Image
-        src={`https://img.etimg.com${data.im}`}
+      <img
+        src={updateURLDimensions(data.im)}
         width={66}
         height={55}
         alt={data.hl}
         loading="lazy"
       />
-    </div>
+    </Link>
   );
 };
