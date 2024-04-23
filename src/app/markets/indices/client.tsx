0124e42ad9bdc06b6053345a24bc9c208d5f9a7e
Ujjwal Paul <ujjwal.paul@timesinternet.in>
2024-04-23 23:23:48 +0530

    interrim commit

--- src/app/markets/indices/client.tsx
+++ src/app/markets/indices/client.tsx
@@ -19,6 +19,26 @@ const IndicesClient = ({
   const [_tableHeaderData, setTableHeaderData] = useState(tableHeaderData);
   const [selectedExchange, setSelectedExchange] = useState(exchange);
   const [processingLoader, setProcessingLoader] = useState(false);
+  const [sortData, setSortData] = useState({ field: null, order: "DESC" });
+
+  const onServerSideSort = useCallback(
+    async (field: any) => {
+      setProcessingLoader(true);
+      setSortData((prevSortData) => {
+        let newSortConfig;
+        if (prevSortData.field === field) {
+          newSortConfig = {
+            field,
+            order: prevSortData.order === "ASC" ? "DESC" : "ASC",
+          };
+        } else {
+          newSortConfig = { field, order: "DESC" };
+        }
+        return newSortConfig;
+      });
+    },
+    [sortData],
+  );
 
   const nseBseMenuSelect = useCallback((e: any) => {
     setProcessingLoader(true);
@@ -27,8 +47,11 @@ const IndicesClient = ({
   }, []);
 
   const updateTableData = async () => {
-    const { tableHeaderData, tableData } =
-      await getAllIndices(selectedExchange);
+    const { tableHeaderData, tableData } = await getAllIndices(
+      selectedExchange,
+      sortData.field,
+      sortData.order,
+    );
     setTableData(tableData);
     setTableHeaderData(tableHeaderData);
     setProcessingLoader(false);
@@ -43,7 +66,7 @@ const IndicesClient = ({
       }
     }, refeshConfig.indicesListing);
     return () => clearInterval(intervalId);
-  }, [selectedExchange, isPrime, currentMarketStatus]);
+  }, [selectedExchange, sortData, isPrime, currentMarketStatus]);
 
   return (
     <>
@@ -77,6 +100,7 @@ const IndicesClient = ({
           }
           tableHeaders={_tableHeaderData}
           tableConfig={tableConfig}
+          handleSortServerSide={onServerSideSort}
           processingLoader={processingLoader}
           isprimeuser={isPrime}
         />
