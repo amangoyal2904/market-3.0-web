9ef596b4af8e8afe5aa118e1cfe4e4bf03b19399
Ujjwal Paul <ujjwal.paul@timesinternet.in>
2024-04-29 19:46:03 +0530

    fiidii interrim commit

--- src/app/markets/fii-dii-activity/fii-fno/page.tsx
+++ src/app/markets/fii-dii-activity/fii-fno/page.tsx
@@ -1,14 +1,30 @@
 import FiiDiiHeader from "@/components/FIIDII/Header";
+import FiiDiiOtherTable from "@/components/FIIDII/OtherTable";
 import FiiDiiTabs from "@/components/FIIDII/Tabs";
-import React from "react";
+import { getFandOCashData } from "@/utils/utility";
 
-const FiiFno = () => {
+const FiiFnO = async () => {
+  const response: any = await getFandOCashData("daily", "index");
+  const { listData, metadataMapping } = response.datainfo.data;
+  const fixedData: any = [];
+  const otherData: any = [];
+  listData.forEach((data: any) => {
+    const { dateLong, datelable, dateStr, ...rest } = data;
+    fixedData.push({ dateLong, datelable, dateStr });
+    otherData.push(rest);
+  });
   return (
     <>
       <FiiDiiHeader />
       <FiiDiiTabs activeTab="fii-fno" />
+      <FiiDiiOtherTable
+        headerTxt="FII F&O Buy-Sell Activity"
+        fixedData={fixedData}
+        otherData={otherData}
+        metadataMapping={metadataMapping}
+      />
     </>
   );
 };
 
-export default FiiFno;
+export default FiiFnO;
