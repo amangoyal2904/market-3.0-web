324ff7f61c2e8c477cc35dbcbf0878cb79072927
Barun Kumar <barun.kumar@timesinternet.in>
2024-05-01 14:47:44 +0530

    _next prefixed for akamai mapping

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: "/marketsweb",
+  rewrites() {
+    return [
+      { source: '/marketsweb/_next/:path*', destination: '/_next/:path*' }
+    ]
+  },
   images: {
     domains: ["img.etimg.com"],
   },
