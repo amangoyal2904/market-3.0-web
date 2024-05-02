9050e5d5b3b1b32d99826eeaae075d862df0f5fa
rohitazad <ram9953878727@gmail.com>
2024-05-02 16:22:11 +0530

    bigbull code do

--- src/components/BigBullSection/InvestorCatModules/FreshEntryCard.tsx
+++ src/components/BigBullSection/InvestorCatModules/FreshEntryCard.tsx
@@ -1,13 +1,26 @@
 import styles from "./styles.module.scss";
 import EntryCard from "./EntryCard";
+import EntryCardViewMore from "./EntryCardViewMore";
+import NodataForTable from "../../BigBullTableCard/NodataForTable";
 
-const FreshEntryCard = ({ cardData }: any) => {
+const FreshEntryCard = ({ cardData, linkTitle = "", linkHref = "" }: any) => {
   return (
-    <div className={styles.cartWraper}>
-      {cardData.map((company: any, index: number) => {
-        return <EntryCard key={`${index}-card`} data={company} />;
-      })}
-    </div>
+    <>
+      {cardData && cardData.length > 0 ? (
+        <div className={styles.cartWraper}>
+          {cardData.map((company: any, index: number) => {
+            return <EntryCard key={`${index}-card`} data={company} />;
+          })}{" "}
+          {linkTitle !== "" && linkHref !== "" && (
+            <EntryCardViewMore linkTitle={linkTitle} linkHref={linkHref} />
+          )}{" "}
+        </div>
+      ) : (
+        <div className={styles.noDataSec}>
+          <NodataForTable />
+        </div>
+      )}
+    </>
   );
 };
 
