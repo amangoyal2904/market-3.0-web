f73f5a5b434a68da0bda98f1e596c2e49e6f6a3b
rohitazad <ram9953878727@gmail.com>
2024-05-06 10:35:47 +0530

    big fixed

--- src/app/markets/top-india-investors-portfolio/[...investor]/investorCatClient.tsx
+++ src/app/markets/top-india-investors-portfolio/[...investor]/investorCatClient.tsx
@@ -36,7 +36,7 @@ const InvestorClientCatePage = ({
   const [sortByChangeHolding, setSortByChangeHolding]: any = useState([
     { label: "All", value: "All" },
     { label: "Increase", value: "Increase" },
-    { label: "Increase", value: "Decrease" },
+    { label: "Decrease", value: "Decrease" },
   ]);
   const [sortByChangeHoldingActive, setSortByChangeHoldingActive] = useState({
     label: "All",
