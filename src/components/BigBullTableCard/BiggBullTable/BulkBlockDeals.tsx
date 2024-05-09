c339944860038970c4617314d7139d5a270bbd35
rohitazad <ram9953878727@gmail.com>
2024-05-09 13:15:03 +0530

    bigbull cr desgin related chagne done

--- src/components/BigBullTableCard/BiggBullTable/BulkBlockDeals.tsx
+++ src/components/BigBullTableCard/BiggBullTable/BulkBlockDeals.tsx
@@ -6,6 +6,11 @@ import Loader from "../../Loader";
 import { useStateContext } from "@/store/StateContext";
 import WatchlistAddition from "../../WatchlistAddition";
 import NodataForTable from "../NodataForTable";
+import { useState, Suspense } from "react";
+import dynamic from "next/dynamic";
+const NonPrimeBlockerModule = dynamic(() => import("../../NonPrimeBlocker"), {
+  ssr: false,
+});
 
 const BiggBullBulkBlockDealsTable = ({
   tableHead,
@@ -16,9 +21,18 @@ const BiggBullBulkBlockDealsTable = ({
 }: any) => {
   //console.log("tableData", tableData);
   const { state } = useStateContext();
-  //const { isPrime } = state.login;
-  const isPrime = true;
+  const { isPrime } = state.login;
+  //const isPrime = true;
   //console.log("isPrime", isPrime);
+  const [showNonPrimeBlocker, setShowNonPrimeBlocker] = useState(false);
+  const blurNameHandler = () => {
+    setShowNonPrimeBlocker(true);
+    document.body.style.overflow = "hidden";
+  };
+  const blurNameHandlerClose = () => {
+    setShowNonPrimeBlocker(false);
+    document.body.style.overflow = "";
+  };
   return (
     <>
       <div className="prel">
@@ -111,7 +125,10 @@ const BiggBullBulkBlockDealsTable = ({
                               </a>
                             </>
                           ) : (
-                            <span className={styles.nameBlur}></span>
+                            <span
+                              className={styles.nameBlur}
+                              onClick={blurNameHandler}
+                            ></span>
                           )}
                         </div>
                       </div>
@@ -149,6 +166,11 @@ const BiggBullBulkBlockDealsTable = ({
         </table>
         {shouldShowLoader && <Loader loaderType="container" />}
       </div>
+      {showNonPrimeBlocker && (
+        <Suspense fallback={<div>Loading</div>}>
+          <NonPrimeBlockerModule oncloseModule={blurNameHandlerClose} />
+        </Suspense>
+      )}
     </>
   );
 };
