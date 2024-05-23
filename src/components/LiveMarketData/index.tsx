cb0edfef43f861ae254f84f644059308dc329a94
Mayur Gupta <maynam26@gmail.com>
2024-05-23 13:05:29 +0530

    page view changes

--- src/components/LiveMarketData/index.tsx
+++ src/components/LiveMarketData/index.tsx
@@ -94,16 +94,17 @@ const LiveMarketData = () => {
                         : "/markets/indices/sp-bse-sensex"
                     }
                   >
-                    {item.indexName == "NIFTY 50" ? "NIFTY" : item.indexName}
+                    {`${item.indexName == "NIFTY 50" ? "NIFTY" : item.indexName} :`}
+                    {/* {item.indexName == "NIFTY 50" ? "NIFTY" : item.indexName} */}
                   </a>
                 </div>
+                {/* <div className={styles.indexContainer}> */}
                 <div className={`${styles.indexValue} numberFonts`}>
                   {formatNumber(item.currentIndexValue)}
                 </div>
                 <div
                   className={`numberFonts ${styles.indexChange} ${Number(item.netChange) > 0 ? styles.green : styles.red}`}
                 >
-                  {item?.percentChange}%
                   <i
                     className={
                       Number(item?.netChange) > 0
@@ -111,7 +112,10 @@ const LiveMarketData = () => {
                         : "eticon_down_arrow"
                     }
                   ></i>
+                  <span>{item.netChange}</span>{" "}
+                  <span>{`(${item?.percentChange}%)`} </span>
                 </div>
+                {/* </div> */}
               </div>
             );
           })}
