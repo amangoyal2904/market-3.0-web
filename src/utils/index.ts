77c7b950284eb667f096595c267ae4a1d04f6e6a
Ujjwal Paul <ujjwal.paul@timesinternet.in>
2024-05-31 18:54:02 +0530

    google client id updated as per LIVE

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -237,8 +237,7 @@ export const initSSOWidget = () => {
     socialLogin: [
       {
         type: "Google",
-        clientId:
-          "891351984915-kodsh6b9vik3h6ue008fh8jgfstageh6.apps.googleusercontent.com",
+        clientId: "936221589938.apps.googleusercontent.com",
       },
       {
         type: "Facebook",
