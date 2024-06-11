7c914c894d627c1e4a446da41db92529baf1a5cb
Ujjwal Paul <ujjwal.paul@timesinternet.in>
2024-06-11 20:14:52 +0530

    absolute url logic change

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,15 @@ const nextConfig = {
   async headers() {
     const headers = [
       {
+        source: "/_next/static/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
         source: "/marketsweb/static/(.*)",
         headers: [
           {
