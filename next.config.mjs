674d84558ff97353bbf4632d6f34b5f7fbc9aefb
Ujjwal Paul <ujjwal.paul@timesinternet.in>
2024-07-11 10:53:04 +0530

    cache config removed

--- next.config.mjs
+++ next.config.mjs
@@ -39,132 +39,132 @@ const nextConfig = {
           },
         ],
       },
-      {
-        source: "/markets/live-coverage",
-        headers: [
-          {
-            key: "Cache-Control",
-            value:
-              "public, max-age=300, s-maxage= 300, must-revalidate, stale-while-revalidate=600", // 5 min
-          },
-          {
-            key: "Expires",
-            value: new Date(Date.now() + 300000).toUTCString(), // 5 min from now
-          },
-        ],
-      },
-      {
-        source: "/markets/stock-market-mood",
-        headers: [
-          {
-            key: "Cache-Control",
-            value:
-              "public, max-age=1200, s-maxage=1200, must-revalidate, stale-while-revalidate=2400", // 20 min
-          },
-          {
-            key: "Expires",
-            value: new Date(Date.now() + 1200000).toUTCString(), // 20 min from now
-          },
-        ],
-      },
-      {
-        source: "/watchlist",
-        headers: [
-          {
-            key: "Cache-Control",
-            value:
-              "public, max-age=1200, s-maxage=1200, must-revalidate, stale-while-revalidate=2400", // 20 min
-          },
-          {
-            key: "Expires",
-            value: new Date(Date.now() + 1200000).toUTCString(), // 20 min from now
-          },
-        ],
-      },
-      {
-        source: "/stocks/marketstats(.*)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value:
-              "public, max-age=1200, s-maxage=1200, must-revalidate, stale-while-revalidate=2400", // 20 min
-          },
-          {
-            key: "Expires",
-            value: new Date(Date.now() + 1200000).toUTCString(), // 20 min from now
-          },
-        ],
-      },
-      {
-        source: "/markets/indices",
-        headers: [
-          {
-            key: "Cache-Control",
-            value:
-              "public, max-age=300, s-maxage= 300, must-revalidate, stale-while-revalidate=600", // 5 min
-          },
-          {
-            key: "Expires",
-            value: new Date(Date.now() + 300000).toUTCString(), // 15 min from now
-          },
-        ],
-      },
-      {
-        source: "/markets/indices/(.*)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value:
-              "public, max-age=900, s-maxage=900, must-revalidate, stale-while-revalidate=1800", // 15 min
-          },
-          {
-            key: "Expires",
-            value: new Date(Date.now() + 900000).toUTCString(), // 15 min from now
-          },
-        ],
-      },
-      {
-        source: "/markets/stock-recos/(.*)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value:
-              "public, max-age=900, s-maxage=900, must-revalidate, stale-while-revalidate=1800", // 15 min
-          },
-          {
-            key: "Expires",
-            value: new Date(Date.now() + 900000).toUTCString(), // 15 min from now
-          },
-        ],
-      },
-      {
-        source: "/markets/stock-screener(.*)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value:
-              "public, max-age=600, s-maxage=600, must-revalidate, stale-while-revalidate=1200", // 10 min
-          },
-          {
-            key: "Expires",
-            value: new Date(Date.now() + 600000).toUTCString(), // 10 min from now
-          },
-        ],
-      },
-      {
-        source: "/markets/top-india-investors-portfolio/(.*)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value:
-              "public, max-age=600, s-maxage=600, must-revalidate, stale-while-revalidate=1200", // 10 min
-          },
-          {
-            key: "Expires",
-            value: new Date(Date.now() + 600000).toUTCString(), // 10 min from now
-          },
-        ],
-      },
+      // {
+      //   source: "/markets/live-coverage",
+      //   headers: [
+      //     {
+      //       key: "Cache-Control",
+      //       value:
+      //         "public, max-age=300, s-maxage= 300, must-revalidate, stale-while-revalidate=600", // 5 min
+      //     },
+      //     {
+      //       key: "Expires",
+      //       value: new Date(Date.now() + 300000).toUTCString(), // 5 min from now
+      //     },
+      //   ],
+      // },
+      // {
+      //   source: "/markets/stock-market-mood",
+      //   headers: [
+      //     {
+      //       key: "Cache-Control",
+      //       value:
+      //         "public, max-age=1200, s-maxage=1200, must-revalidate, stale-while-revalidate=2400", // 20 min
+      //     },
+      //     {
+      //       key: "Expires",
+      //       value: new Date(Date.now() + 1200000).toUTCString(), // 20 min from now
+      //     },
+      //   ],
+      // },
+      // {
+      //   source: "/watchlist",
+      //   headers: [
+      //     {
+      //       key: "Cache-Control",
+      //       value:
+      //         "public, max-age=1200, s-maxage=1200, must-revalidate, stale-while-revalidate=2400", // 20 min
+      //     },
+      //     {
+      //       key: "Expires",
+      //       value: new Date(Date.now() + 1200000).toUTCString(), // 20 min from now
+      //     },
+      //   ],
+      // },
+      // {
+      //   source: "/stocks/marketstats(.*)",
+      //   headers: [
+      //     {
+      //       key: "Cache-Control",
+      //       value:
+      //         "public, max-age=1200, s-maxage=1200, must-revalidate, stale-while-revalidate=2400", // 20 min
+      //     },
+      //     {
+      //       key: "Expires",
+      //       value: new Date(Date.now() + 1200000).toUTCString(), // 20 min from now
+      //     },
+      //   ],
+      // },
+      // {
+      //   source: "/markets/indices",
+      //   headers: [
+      //     {
+      //       key: "Cache-Control",
+      //       value:
+      //         "public, max-age=300, s-maxage= 300, must-revalidate, stale-while-revalidate=600", // 5 min
+      //     },
+      //     {
+      //       key: "Expires",
+      //       value: new Date(Date.now() + 300000).toUTCString(), // 15 min from now
+      //     },
+      //   ],
+      // },
+      // {
+      //   source: "/markets/indices/(.*)",
+      //   headers: [
+      //     {
+      //       key: "Cache-Control",
+      //       value:
+      //         "public, max-age=900, s-maxage=900, must-revalidate, stale-while-revalidate=1800", // 15 min
+      //     },
+      //     {
+      //       key: "Expires",
+      //       value: new Date(Date.now() + 900000).toUTCString(), // 15 min from now
+      //     },
+      //   ],
+      // },
+      // {
+      //   source: "/markets/stock-recos/(.*)",
+      //   headers: [
+      //     {
+      //       key: "Cache-Control",
+      //       value:
+      //         "public, max-age=900, s-maxage=900, must-revalidate, stale-while-revalidate=1800", // 15 min
+      //     },
+      //     {
+      //       key: "Expires",
+      //       value: new Date(Date.now() + 900000).toUTCString(), // 15 min from now
+      //     },
+      //   ],
+      // },
+      // {
+      //   source: "/markets/stock-screener(.*)",
+      //   headers: [
+      //     {
+      //       key: "Cache-Control",
+      //       value:
+      //         "public, max-age=600, s-maxage=600, must-revalidate, stale-while-revalidate=1200", // 10 min
+      //     },
+      //     {
+      //       key: "Expires",
+      //       value: new Date(Date.now() + 600000).toUTCString(), // 10 min from now
+      //     },
+      //   ],
+      // },
+      // {
+      //   source: "/markets/top-india-investors-portfolio/(.*)",
+      //   headers: [
+      //     {
+      //       key: "Cache-Control",
+      //       value:
+      //         "public, max-age=600, s-maxage=600, must-revalidate, stale-while-revalidate=1200", // 10 min
+      //     },
+      //     {
+      //       key: "Expires",
+      //       value: new Date(Date.now() + 600000).toUTCString(), // 10 min from now
+      //     },
+      //   ],
+      // },
     ];
 
     return headers;
