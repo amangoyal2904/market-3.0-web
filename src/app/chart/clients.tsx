24172f6e48621efc17798f9d95d9cecc08f5e2f6
Ujjwal Paul <ujjwal.paul@timesinternet.in>
2024-08-06 15:14:49 +0530

    technical chart changes

---
+++ src/app/chart/clients.tsx
@@ -0,0 +1,29 @@
+"use client";
+import dynamic from "next/dynamic";
+import { useState } from "react";
+import Script from "next/script";
+
+const TVChartContainer = dynamic(
+  () =>
+    import("@/components/TVChartContainer").then((mod) => mod.TVChartContainer),
+  { ssr: false },
+);
+
+const ChartClient = (defaultWidgetProps: any) => {
+  const [isScriptReady, setIsScriptReady] = useState(false);
+
+  return (
+    <>
+      <Script
+        src="/marketsweb/static/v27/datafeeds/udf/dist/bundle.js"
+        strategy="lazyOnload"
+        onReady={() => {
+          setIsScriptReady(true);
+        }}
+      />
+      {isScriptReady && <TVChartContainer {...defaultWidgetProps} />}
+    </>
+  );
+};
+
+export default ChartClient;
