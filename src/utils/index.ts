50bb5fba3d973726a474472d8af9c2a6eabd5f39
Mayur Gupta <maynam26@gmail.com>
2024-08-12 13:37:50 +0530

    path and domain print

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -94,7 +94,7 @@ export const setCookieToSpecificTime = (
       const expirationDate = new Date(Date.now() + seconds * 1000);
       cookiestring += `expires=${expirationDate.toUTCString()};`;
     }
-    console.log("Domain for Cookie-----", options.domain);
+    console.log("Domain for Cookie-----", options.domain, options.path);
     cookiestring += `domain=${options.domain}; path=${options.path};`;
 
     document.cookie = cookiestring;
